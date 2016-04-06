bdd71f147af8e6f717ffbf6484e2058d2ed9fc99
Thomas Tkocz <thomas.tkocz@gmail.com>
2016-04-06 20:17:07 +0200

    gas sgation assessment checkin + adaptations

---
+++ gasstation-assessment/src/main/java/net/bigpoint/assessment/gasstation/GasPump.java
@@ -0,0 +1,53 @@
+package net.bigpoint.assessment.gasstation;
+
+/**
+ * This class is an implementation of a gas pump.
+ * 
+ * It is final and should not be modified!
+ * 
+ * It is not thread-safe! It should only ever be used by one thread.
+ * 
+ * Especially only one thread at a time may call the pumpGas(double) method!
+ * 
+ */
+public final class GasPump {
+
+	private final GasType gasType;
+	private double price;
+	private double amount;
+
+	public GasPump(GasType gasType, double amount, double price) {
+		super();
+		this.gasType = gasType;
+		this.price = price;
+		this.amount = amount;
+	}
+
+	public void pumpGas(double amount) {
+		this.amount -= amount;
+
+		// simulate that it takes time to pump some gas
+		try {
+			Thread.sleep((long) (amount * 100));
+		} catch (InterruptedException e) {
+			// ignored
+		}
+	}
+
+	public double getRemainingAmount() {
+		return amount;
+	}
+
+	public GasType getGasType() {
+		return gasType;
+	}
+
+	public void setGasPrice(double price) {
+		this.price = price;
+	}
+
+	public double getGasPrice() {
+		return this.price;
+	}
+
+}
---
+++ gasstation-assessment/src/main/java/net/bigpoint/assessment/gasstation/exceptions/GasNotAvailableException.java
@@ -0,0 +1,10 @@
+package net.bigpoint.assessment.gasstation.exceptions;
+
+public class GasNotAvailableException extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7319119260359273226L;
+
+}
